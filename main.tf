bc24e315d2031176c0af4787741ac735fdd8795a
prabin <email@email.com>
2023-03-15 13:37:33 +0000

    azure test

--- main.tf
+++ main.tf
@@ -1,35 +1,21 @@
+# Configure the Azure provider
 terraform {
   required_providers {
-    null = {
-      source = "hashicorp/null"
-      version = "3.1.1"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
     }
   }
+
+  required_version = ">= 1.1.0"
 }
 
-provider "null" {
-  # Configuration options
+provider "azurerm" {
+  features {}
 }
 
-/* a list of local variables */
-locals {
-  jones = ["indy", "henry", "marian", "katanga"]
-}
-
-
-/* The null_resource implements the standard resource lifecycle but takes no more action */
-resource "null_resource" "jones" {
-  for_each = toset(local.jones)
-  /* triggers allows specifying a random set of values that when
-     changed will cause the resource to be replaced */
-  triggers = {
-    name = each.value  // a special variable, "each" created by terraform
-                       // the object has "each.key" and "each.value"
-  }
-}
-
-/* We want these outputs */
-output "jones" {
-  value = null_resource.jones
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
 }
 
